b380330da089888976daa90cb9f53e460aa6e907
Felix <boyboby@163.com>
2018-10-23 11:25:55 +0800

    Initial Commit

---
+++ Faith/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Faith
+//
+//  Created by 潘磊 on 2018/10/23.
+//  Copyright © 2018 PL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
